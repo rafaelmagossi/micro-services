e54339005181fbadfb61e91acfcf16227e72f263
Rafael Magossi <rafael_mq2@hotmail.com>
2019-06-23 21:43:11 -0300

    Insert default data on H2 database

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER VALUES(10001, '1989-02-07', 'Rafael Magossi');
+INSERT INTO USER VALUES(10002, sysdate(), 'Gabriel Kirsten');
+INSERT INTO USER VALUES(10024, sysdate(), 'Matheus Lino');
